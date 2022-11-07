c90d956f5571b7e4c5804d33709a3b6bac1c3ae0
Param <param23403@gmail.com>
2022-11-06 21:29:09 -0500

    minorChanges

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.backend_bases import MouseButton
+
 
 # Reading the Excel file
 var = pd.read_excel("2023SoftwareInternAssignment.xlsx")
 
 # declaring  x and y arrays for data storage
-x=list(var['ReferenceDate'])
-y=[]
+x = list(var['ReferenceDate'])
+y = []
 
 # dailyReturn% + 1 stored in temporary array
-temp=list(var['DailyReturn']/100+1)
+temp = list(var['DailyReturn']/100+1)
 
-#This function does our total Return Calculation
+# This function does our total Return Calculation
 def product(ending):
-    result=1
-    for i in range(0,ending):
-        result*=temp[i]
+    result = 1
+    for i in range(0, ending):
+        result *= temp[i]
     return (result-1)*100
 
-#We calculate total return for all of our data points
-for i in range(1,len(temp)+1):
+# We calculate total return for all of our data points
+for i in range(1, len(temp)+1):
     y.append(product(i))
 
-#Creating and initializing the line graph
-plt.figure(figsize=(10,10))
+# Creating and initializing the line graph
+plt.figure(figsize=(10, 10))
 plt.style.use('_mpl-gallery')
 plt.plot(x, y, linewidth=1.0)
-plt.title("S&P 500 Daily Return")
+plt.ylabel('Total Return', fontweight="bold")
+plt.xlabel('Date', fontweight="bold")
+plt.title("S&P 500 Daily Return", fontweight="bold")
 plt.show()
