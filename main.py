576eccbca89b5c482f57df3c37b0e78003a6ee81
Param <param23403@gmail.com>
2022-11-06 20:31:14 -0500

    initialGraphCreated

---
+++ main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Reading the Excel file
+var = pd.read_excel("2023SoftwareInternAssignment.xlsx")
+
+# declaring  x and y arrays for data storage
+x=list(var['ReferenceDate'])
+y=[]
+
+# dailyReturn% + 1 stored in temporary array
+temp=list(var['DailyReturn']/100+1)
+
+#This function does our total Return Calculation
+def product(ending):
+    result=1
+    for i in range(0,ending):
+        result*=temp[i]
+    return (result-1)*100
+
+#We calculate total return for all of our data points
+for i in range(1,len(temp)+1):
+    y.append(product(i))
+
+#Creating and initializing the line graph
+plt.figure(figsize=(10,10))
+plt.style.use('_mpl-gallery')
+plt.plot(x, y, linewidth=1.0)
+plt.title("S&P 500 Daily Return")
+plt.show()
